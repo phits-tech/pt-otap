03098ca9d94d76a69b032728f14a824ba27caded
Antony Harfield <antonyharfield@gmail.com>
2021-03-06 16:21:29 +0700

    Add workshop detail

--- pages/workshops.vue
+++ pages/workshops.vue
@@ -5,40 +5,62 @@
         <h2 class="subtitle">
           กิจกรรมต่อไป
         </h2>
-        <h1 class="title">
-          Bootcamp 1: Validation ตรวจสอบความถูกต้อง เหมาะสมของผลิตภัณฑ์
-        </h1>
-        <div class="level">
-          <div class="level-item-left">
-            <div class="is-inline-block mr-5 has-text-centered">
-              <figure class="image is-96x96 is-inline-block">
-                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker1.jpg">
-              </figure>
-              <div class="has-text-weight-bold has-text-grey">
-                รศ อรพิณ สันติธีรากุล
-              </div>
+        <div class="columns">
+          <div class="column">
+            <h1 class="title">
+              BMC &amp; TEC Consulting การประเมินแผนธุรกิจและเทคโนโลยี
+            </h1>
+            <div class="content is-size-5">
+              <b-icon icon="calendar" /> 10 มีนาคม 2564<br>
+              <b-icon icon="clock" /> 9:00-18:00 (by appointment)<br>
+              <b-icon icon="pin" /> SciPark มหาวิทยาลัยนเรศวร
             </div>
-            <div class="is-inline-block mr-5 has-text-centered">
-              <figure class="image is-96x96 is-inline-block">
-                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker2.jpg">
+          </div>
+          <div class="column">
+            <div class="is-flex is-align-items-center mb-4">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/porramate.jpg">
               </figure>
-              <div class="has-text-weight-bold has-text-grey">
-                ดร ศันสนา สิริตาม
-              </div>
-            </div>
-            <div class="is-inline-block has-text-centered">
-              <figure class="image is-96x96 is-inline-block">
-                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker3.jpg">
-              </figure>
-              <div class="has-text-weight-bold has-text-grey">
-                อ สุพจน์ กุลปรางค์ทอง
+              <div class="has-text-grey">
+                <span class="is-size-4">
+                  ดร ปรเมษฐ์ ชุ่มยิ้ม
+                </span><br>
+                วิทยาลัยนานาชาติมหาวิทยาลัยมหิดล
               </div>
             </div>
           </div>
-          <div class="level-item-right">
+        </div>
+
+        <div class="columns mt-5">
+          <div class="column">
+            <h1 class="title">
+              Bootcamp 2: Agile Mindset and Scrum
+            </h1>
             <div class="content is-size-5">
-              <b-icon icon="calendar" /> 27-28 กุมภาพันธ์ 2564<br>
-              <b-icon icon="pin" /> โรงแรมชินะปุระ
+              <b-icon icon="calendar" /> 13-14 มีนาคม 2564<br>
+              <b-icon icon="clock" /> 9:00-17:00<br>
+              <b-icon icon="pin" /> โรงแรม Grand Riverside
+            </div>
+          </div>
+          <div class="column">
+            <div class="is-flex is-align-items-center mb-4">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/workshop2-speaker1.jpg">
+              </figure>
+              <div class="has-text-grey">
+                <span class="is-size-5">
+                  คุณ ทวิร พานิชสมบัติ
+                </span><br>
+                <a href="https://medium.com/odds-team">ODDS</a>
+              </div>
+            </div>
+            <div class="is-flex is-align-items-center">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <b-icon icon="account-group" size="is-large" style="color: #aaa; background-color: #ddd; border-radius: 24px; overflow: hidden;" />
+              </figure>
+              <span class="has-text-grey is-size-5">
+                &amp; team
+              </span>
             </div>
           </div>
         </div>
@@ -56,16 +78,6 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>10 มีนาคม 2564</td>
-              <td>การประเมินแผนธุรกิจ และเทคโนโลยี (BMC &amp; TEC)</td>
-              <td>ดร ปรเมษฐ์ ชุ่มยิ้ม</td>
-            </tr>
-            <tr>
-              <td>13-14 มีนาคม 2564</td>
-              <td>Bootcamp 2: Agile Mindset and Scrum</td>
-              <td>คุณ ทวิร พานิชสมบัติ</td>
-            </tr>
             <tr>
               <td>3-4 เมษายน 2564</td>
               <td>Bootcamp 3: Design, UX and UI</td>
@@ -89,35 +101,78 @@
           กิจกรรมที่ผ่านมา
         </h2>
 
-        <h3 class="title">
-          OTAP Launch &amp; Orientation
-        </h3>
-        <div class="level">
-          <div class="level-item-left">
-            <div class="is-inline-block mr-5 has-text-centered">
-              <figure class="image is-96x96 is-inline-block">
-                <img class="is-rounded" src="~/assets/images/people/workshop0-speaker1.jpg">
-              </figure>
-              <div class="has-text-grey">
-                <span class="has-text-weight-bold">Kittichai Phiphatbunyarat</span><br>
-                <a href="https://apitel.co">Apitel.co</a> / <a href="https://chiangmaiandco.org">Chiangmai&amp;co</a> / ARISTO
-              </div>
-            </div>
-            <div class="is-inline-block mr-5 has-text-centered">
-              <figure class="image is-96x96 is-inline-block">
-                <img class="is-rounded" src="~/assets/images/people/sukhgij.jpg">
-              </figure>
-              <div class="has-text-grey">
-                <span class="has-text-weight-bold">Sukhgij Ysothonsreekul, Ph.D.</span><br>
-                OTAP / Naresuan University
-              </div>
+        <div class="columns mb-5">
+          <div class="column">
+            <h1 class="title">
+              Bootcamp 1: Validation ตรวจสอบความถูกต้อง เหมาะสมของผลิตภัณฑ์
+            </h1>
+            <div class="content is-size-5">
+              <b-icon icon="calendar" /> 27-28 กุมภาพันธ์ 2564<br>
+              <b-icon icon="clock" /> 9:00-17:00<br>
+              <b-icon icon="pin" /> โรงแรมชินะปุระ
             </div>
           </div>
-          <div class="level-item-right">
+          <div class="column">
+            <div class="is-flex is-align-items-center mb-2">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker1.jpg">
+              </figure>
+              <span class="has-text-grey is-size-4">
+                รศ อรพิณ สันติธีรากุล
+              </span>
+            </div>
+            <div class="is-flex is-align-items-center mb-2">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker2.jpg">
+              </figure>
+              <span class="has-text-grey is-size-4">
+                ดร ศันสนา สิริตาม
+              </span>
+            </div>
+            <div class="is-flex is-align-items-center">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/workshop1-speaker3.jpg">
+              </figure>
+              <span class="has-text-grey is-size-4">
+                อ สุพจน์ กุลปรางค์ทอง
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="columns mb-5">
+          <div class="column">
+            <h1 class="title">
+              OTAP Launch &amp; Orientation
+            </h1>
             <div class="content is-size-5">
               <b-icon icon="calendar" /> 20 กุมภาพันธ์ 2564<br>
               <b-icon icon="clock" /> 9:00-14:00<br>
               <b-icon icon="pin" /> สเต็กปีนัง Penang Steak Brasserie<br>
+            </div>
+          </div>
+          <div class="column">
+            <div class="is-flex is-align-items-center mb-4">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/workshop0-speaker1.jpg">
+              </figure>
+              <div class="has-text-grey">
+                <span class="is-size-5">
+                  Kittichai Phiphatbunyarat
+                </span><br>
+                <a href="https://apitel.co">Apitel.co</a> / <a href="https://chiangmaiandco.org">Chiangmai&amp;co</a> / ARISTO
+              </div>
+            </div>
+            <div class="is-flex is-align-items-center">
+              <figure class="image is-48x48 is-inline-block mr-2">
+                <img class="is-rounded" src="~/assets/images/people/sukhgij.jpg">
+              </figure>
+              <div class="has-text-grey">
+                <span class="is-size-5">
+                  Sukhgij Ysothonsreekul, Ph.D.
+                </span><br>
+                OTAP / Naresuan University
+              </div>
             </div>
           </div>
         </div>
