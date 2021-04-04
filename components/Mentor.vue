5900bc7f69dce2b5371fa579f02ca502c34cdd0a
Antony Harfield <antonyharfield@gmail.com>
2021-04-04 14:12:54 +0700

    Add mentors and link to request mentor

---
+++ components/Mentor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-square">
+        <img :src="imageSrc" alt="Photo of Apirak">
+      </figure>
+    </div>
+    <div class="card-content">
+      <p class="title is-4">
+        {{ name }}
+      </p>
+      <p class="subtitle is-6">
+        {{ topics }}
+      </p>
+      <p class="content">
+        <slot />
+        <a v-if="linkedin" :href="'https://www.linkedin.com/in/' + linkedin + '/'"><b-icon
+          icon="linkedin"
+          style="margin-top: -3px;"
+        /></a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Card extends Vue {
+  @Prop()
+  name!: string
+
+  @Prop()
+  imageSrc!: string
+
+  @Prop()
+  topics!: string
+
+  @Prop()
+  linkedin!: string
+}
+</script>
--- pages/mentors.vue
+++ pages/mentors.vue
@@ -5,9 +5,127 @@
         <h1 class="title">
           Mentors
         </h1>
-        <p class="has-text-grey is-size-5">
-          จะประกาศ 1 มีนาคม 2564
-        </p>
+        <div class="notification is-link is-light has-text-centered">
+          <div class="is-5 mb-3">
+            For IGNITE teams, please complete the form to request a mentor.
+          </div>
+          <div>
+            <a class="button is-link" href="https://docs.google.com/forms/d/e/1FAIpQLSe04AbChG3skk1XebBh5rrwv76gSNy53Hi2-Mh83reqOhRT9g/viewform">
+              Request A Mentor
+            </a>
+          </div>
+        </div>
+        <div class="columns is-multiline">
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/workshop3-speaker1.jpg')"
+              name="Apirak Panatkool"
+              topics="UX/UI Design"
+              linkedin="apirak"
+            >
+              Founder of UX Academy and UX Thailand. UX Coach at ODDS.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/pornpat.jpg')"
+              name="Pornpat Pimjaroen"
+              topics="UX/UI Design"
+              linkedin="pornpat-pimjaroen"
+            >
+              UX Lead at Krungthai.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/workshop2-speaker1.jpg')"
+              name="Twin Panitsombat"
+              topics="Agile, Scrum"
+            >
+              Founder of ODDS. Agile Coach.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/supatida.jpg')"
+              name="Supatida Prompayuck"
+              topics="Marketing, Strategy"
+              linkedin="supatida"
+            >
+              Founder of The S Curve. Women Techmakers Ambassador Thailand.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/workshop2-speaker2.jpg')"
+              name="Phongsak Ritpitakphong"
+              topics="Agile, Scrum"
+            >
+              Agile Coach at ODDS.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/workshop0-speaker1.jpg')"
+              name="Kittichai Phiphatbunyarat"
+              topics="Strategy"
+              linkedin="kittichai"
+            >
+              Apitel.co / Chiangmai&co / ARISTO
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/porramate.jpg')"
+              name="Porramate Chumyim"
+              topics="Strategy, Marketing Analytics"
+              linkedin="porramate-chumyim-b8257112b"
+            >
+              ที่ปรึกษาอาวุโส Food Innopolis
+              สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ (สวทช)
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/wansuree.jpg')"
+              name="Wansuree Massagram"
+              topics="IoT, Hardware"
+              linkedin="wansuree-massagram-43776259"
+            >
+              Founder of NU Maker Club. Lecturer at Naresuan University.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/ekkasit.jpg')"
+              name="Ekkasit Tiamkaew"
+              topics="Online Marketing, Branding, Content Marketing"
+            >
+              Online Marketing, Branding, Content Marketing
+            </mentor>
+          </div>
+
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/somjintana.jpg')"
+              name="Somjintana Korbut"
+              topics="Mobile (iOS), Web, Firebase"
+              linkedin="somjintana-korbut-7691b4bb"
+            >
+              Google Developer Expert in Firebase. Mobile Lead at Rainforest Connection.
+            </mentor>
+          </div>
+          <div class="column is-one-quarter">
+            <mentor
+              :image-src="require('~/assets/images/people/charles.jpg')"
+              name="Charles Allen"
+              topics="Agile, Web, Mobile (Android), Chatbots"
+              linkedin="ajahncharles"
+            >
+              Lecturer at Naresuan University.
+            </mentor>
+          </div>
+        </div>
       </div>
     </section>
   </div>
