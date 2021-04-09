ee0bb194fc68b39b5be46d3ab03a6164868511eb
Sopaphorn <82072741+Sopaphorn@users.noreply.github.com>
2021-04-09 10:02:30 +0700

    add code
    add code thai name and mentor.vue add thai name

---
+++ lang/th/pages.ts
@@ -0,0 +1,3 @@
+export const mentor = {
+    title: "เมนเทอร์"
+}
--- pages/mentors.vue
+++ pages/mentors.vue
@@ -20,6 +20,7 @@
             <mentor
               :image-src="require('~/assets/images/people/workshop3-speaker1.jpg')"
               name="Apirak Panatkool"
+              thaiName="อภิรักษ์ ชุ่มยิ้ม"
               topics="UX/UI Design"
               linkedin="apirak"
             >
@@ -30,6 +31,7 @@
             <mentor
               :image-src="require('~/assets/images/people/pornpat.jpg')"
               name="Pornpat Pimjaroen"
+              thaiName="พรภัทร พิมพ์เจริญ"
               topics="UX/UI Design"
               linkedin="pornpat-pimjaroen"
             >
@@ -40,6 +42,7 @@
             <mentor
               :image-src="require('~/assets/images/people/workshop2-speaker1.jpg')"
               name="Twin Panitsombat"
+              thaiName="ทวิร พานิชสมบัติ"
               topics="Agile, Scrum"
             >
               Founder of ODDS. Agile Coach.
@@ -49,6 +52,7 @@
             <mentor
               :image-src="require('~/assets/images/people/supatida.jpg')"
               name="Supatida Prompayuck"
+              thaiName="ศุภธิดา พรหมพยัคฆ์"
               topics="Marketing, Strategy"
               linkedin="supatida"
             >
@@ -59,6 +63,7 @@
             <mentor
               :image-src="require('~/assets/images/people/workshop2-speaker2.jpg')"
               name="Phongsak Ritpitakphong"
+              thaiName="พงษ์ศักดิ์ ฤทธิ์พิทักษ์พงศ์"
               topics="Agile, Scrum"
             >
               Agile Coach at ODDS.
@@ -68,6 +73,7 @@
             <mentor
               :image-src="require('~/assets/images/people/workshop0-speaker1.jpg')"
               name="Kittichai Phiphatbunyarat"
+              thaiName="กิตติชัย พิพัฒน์บุณยารัตน์"
               topics="Strategy"
               linkedin="kittichai"
             >
@@ -78,6 +84,7 @@
             <mentor
               :image-src="require('~/assets/images/people/porramate.jpg')"
               name="Porramate Chumyim"
+              thaiName="ปรเมษฐ์ ชุ่มยิ้ม"
               topics="Strategy, Marketing Analytics"
               linkedin="porramate-chumyim-b8257112b"
             >
@@ -89,6 +96,7 @@
             <mentor
               :image-src="require('~/assets/images/people/wansuree.jpg')"
               name="Wansuree Massagram"
+              thaiName="วันสุรีย์ มาศกรัม"
               topics="IoT, Hardware"
               linkedin="wansuree-massagram-43776259"
             >
@@ -99,6 +107,7 @@
             <mentor
               :image-src="require('~/assets/images/people/ekkasit.jpg')"
               name="Ekkasit Tiamkaew"
+              thaiName="เอกสิทธิ์ เทียมแก้ว"
               topics="Online Marketing, Branding, Content Marketing"
             >
               Online Marketing, Branding, Content Marketing
@@ -109,6 +118,7 @@
             <mentor
               :image-src="require('~/assets/images/people/somjintana.jpg')"
               name="Somjintana Korbut"
+              thaiName="สมจินตนา กอบุตร"
               topics="Mobile (iOS), Web, Firebase"
               linkedin="somjintana-korbut-7691b4bb"
             >
