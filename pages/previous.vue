a20d84174f706007d2190324c8b74871c73fef74
MIKE <93254568+KritsanaMike@users.noreply.github.com>
2023-08-22 17:23:03 +0700

    Update previous.vue

--- pages/previous.vue
+++ pages/previous.vue
@@ -242,9 +242,7 @@
               <b-icon icon="calendar" /> 10-12 พฤศจิกายน 2565<br />
               <b-icon icon="clock" /> 08.00 - 17.00 <br />
               <b-icon icon="pin" />
-              <a href="https://th.mosaic-collection.com/sukhothai-heritage/"
-                >โรงแรมสุโขทัยเฮอริเทจรีสอร์ท จังหวัดสุโขทัย</a
-              >
+              <a href="https://th.mosaic-collection.com/sukhothai-heritage/">โรงแรมสุโขทัยเฮอริเทจรีสอร์ท จังหวัดสุโขทัย</a>
               <br />
             </div>
           </div>
@@ -450,9 +448,7 @@
               <b-icon icon="calendar" /> 3-4 เมษายน 2564<br />
               <b-icon icon="clock" /> 9:00-17:00<br />
               <b-icon icon="pin" />
-              <a href="https://www.mosaic-collection.com/sukhothai-heritage/"
-                >Sukhothai Heritage Resort</a
-              >
+              <a href="https://www.mosaic-collection.com/sukhothai-heritage/">Sukhothai Heritage Resort</a>
             </div>
           </div>
           <div class="column">
