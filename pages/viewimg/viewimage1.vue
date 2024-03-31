32abbcbbbfe088de4920ed6a7527d0666047155b
MIKE <93254568+KritsanaMike@users.noreply.github.com>
2024-03-31 14:14:51 +0700

    update

---
+++ pages/viewimg/viewimage1.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="posts">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-10 is-offset-1">
+          <div class="columns featured-post is-multiline">
+            <div class="column is-12 post center">
+              <br />
+              <h5 class="title post-title">
+                {{ titles }}
+              </h5>
+            </div>
+          </div>
+          <hr />
+          <div class="columns is-multiline">
+            <div
+              class="column post is-6"
+              v-for="index in numberList"
+              :key="index"
+            >
+              <article class="columns is-multiline">
+                <div class="column is-12 featured-content">
+                  <div class="column is-12 post-img">
+                    <img :src="getImagePath(index)" :alt="`Image ${index}`" />
+                  </div>
+                  <br />
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  layout: '',
+  data () {
+    let totalImage = 59
+    let imageDir = 'viewimg1'
+    let imageLast = 'jpeg'
+    let titles = 'Thailand’s Taste of Tomorrow: Moving Beyond The Plate'
+    return { a: totalImage, imageDir, imageLast, titles }
+  },
+  computed: {
+    numberList () {
+      return Array.from({ length: this.a }, (_, index) => index + 1)
+    }
+  },
+  methods: {
+    getImagePath (index) {
+      return require(`@/assets/${this.imageDir}/${index}.${this.imageLast}`)
+    }
+  }
+}
+</script>
---
+++ pages/viewimg/viewimage4.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="posts">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-10 is-offset-1">
+          <div class="columns featured-post is-multiline">
+            <div class="column is-12 post center">
+              <br />
+              <h5 class="title post-title">
+                {{ titles }}
+              </h5>
+            </div>
+          </div>
+          <hr />
+          <div class="columns is-multiline">
+            <div
+              class="column post is-6"
+              v-for="index in numberList"
+              :key="index"
+            >
+              <article class="columns is-multiline">
+                <div class="column is-12 featured-content">
+                  <div class="column is-12 post-img">
+                    <img :src="getImagePath(index)" :alt="`Image ${index}`" />
+                  </div>
+                  <br />
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  layout: '',
+  data () {
+    let totalImage = 47
+    let imageDir = 'viewimg4'
+    let imageLast = 'jpg'
+    let titles = 'Innovative Local Food for NCDs'
+    return { a: totalImage, imageDir, imageLast, titles }
+  },
+  computed: {
+    numberList () {
+      return Array.from({ length: this.a }, (_, index) => index + 1)
+    }
+  },
+  methods: {
+    getImagePath (index) {
+      return require(`@/assets/${this.imageDir}/${index}.${this.imageLast}`)
+    }
+  }
+}
+</script>
